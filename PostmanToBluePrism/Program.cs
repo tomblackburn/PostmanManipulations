bbdad56685243c5ee9894104e12d538c03417e30
Tom Blackburn <hello.tomblackburn@gmail.com>
2018-08-31 13:21:20 +0100

    Read JSON Contents
    Added System.IO namespace, using File.ReadAllText(string path) to read in the JSON from a file location

--- PostmanToBluePrism/Program.cs
+++ PostmanToBluePrism/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace PostmanToBluePrism
 {
@@ -6,7 +7,26 @@ namespace PostmanToBluePrism
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // Read .Json file content from a file location into a variable
+            var json = File.ReadAllText(@"C:\Core\Thoughtonomy\Products\Postman\Transunion.postman_collection.json");
+
+            // Parse that JSON into our Classes/Models
+
+            // Validate Models?
+
+            // Prepare BP Object Boilerplate XML
+
+            // Cleabnse BP Object Boilerplate with randomly generated GUIDs
+
+            // Foreach item that has been pased from JSON, Create a Subsheet or Page XML
+
+            // Read necessary data from models and generate the action workflow structure (Utility - HTTP and JSON to Collection)
+
+            // Write Xml in a directory for the user to import
+
+            // Could import?
+
         }
     }
 }
